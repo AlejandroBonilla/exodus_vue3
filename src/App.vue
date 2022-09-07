d6598a93633a000746aeda11799dfa1d7384905f
Alejandro Bonilla <abonilla@edutecno.com>
2022-09-07 17:55:23 -0400

    vue 3 + vue router4

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'appVue',
+  // props: {},
+  data: function(){
+    return {}
+  },
+  // computed: {},
+  //methods: {}
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+  
+</style>
